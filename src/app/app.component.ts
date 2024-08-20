1d961093c70f711275e5f207c5997267e5582899
Jabale Nur Adnan <jabalenuradnan6@gmail.com>
2024-08-20 20:25:50 +0600

    commented dist in gitignore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PreviewBoardComponent } from "./preview-board/preview-board.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PreviewBoardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'PhotoGallery';
+
+
 }
